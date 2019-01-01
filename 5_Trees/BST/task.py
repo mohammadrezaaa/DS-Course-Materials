c938095344b6a8ba3d62f6c1a3eb77890f4af919
mohammadrezaaa <mr.ahmadkhaniha@gmail.com>
2019-01-01 18:12:09 +0330

    BST_task1

--- 5_Trees/BST/task.py
+++ 5_Trees/BST/task.py
@@ -1,1 +1,31 @@
-# TODO: type solution here
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+prev = None
+
+def isbst(root):
+    global prev
+    prev = None
+    return isbst_mainfunc(root)
+
+def isbst_mainfunc(root):
+    global prev
+
+    if root is None:
+        return True
+
+    if isbst_mainfunc(root.left) is False:
+        return False
+
+    if prev is not None and prev.value > root.value:
+        return False
+
+    prev = root
+    return isbst_mainfunc(root.right)
+
+
